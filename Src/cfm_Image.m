bbf0293e8761b4ab4b6a932d82772fc61856f681
warmmilk21 <728812751@qq.com>
2017-05-31 22:35:26 +0800

    增加速度预估计部分
    模值计算部分待修改

--- Src/cfm_Image.m
+++ Src/cfm_Image.m
@@ -1,91 +1,112 @@
 clc;clear
+%% parameters set manual
+theta = 15/180*pi; %deflection angle
+WF_Order = 4;      %wall filter order
+line_density = 2;  %1-high density;2-middle density;4-low density
+scale = 23.1;      %the upper limit velocity (cm/s) 
+Reverse = 1;       %1-left deflection;0-right deflection 
+FenceRemove = 1;   %if remove fence line
 
-%parameters set manual
-c = 1540;
-f0 = 5e6;
-fprf = 3.0e3;
-theta = 15/180*pi;
-scale = 23.1;  %cm/s 
-DB_range = 60;
-line_density = 2;
-WF_Order = 4;
-ROthre = 50;
+DB_range_min = -12;
+DB_range_max = 12;
+RO_thre = 50;
 
-FRthre = 32;
+Modulus_thre = 32000;  %modulus threshold
+
+FR_thre = 32;    %flash reject threshold
+%% 
 load cfm_colormap.mat
 cfm_clrmap=cfm_clrmap0./256;
+if (Reverse==1)
+    cfm_clrmap = flipud(cfm_clrmap);
+end
+
+if (FenceRemove==1)
+    ens_start = 2;
+else
+    ens_start = 1;
+end
 
 pathname = '../data/';
-for idx=1:10
-    filename = ['c_linedata',num2str(idx),'.mat'];
+for frm_idx=1:10
+    filename = ['c_linedata',num2str(frm_idx),'.mat'];
     load ([pathname filename]);
     [sampleCnt,lineCnt,enssembleNum] = size(frmI);
-
+%% PreEstimate
+     %remove DC
+     SumI = zeros(sampleCnt,lineCnt);
+     SumQ = zeros(sampleCnt,lineCnt);
+     for idx=ens_start:enssembleNum
+         SumI = SumI + frmI(:,:,idx);
+         SumQ = SumQ + frmQ(:,:,idx);
+     end
+     SumI = SumI./(enssembleNum-ens_start+1);
+     SumQ = SumQ./(enssembleNum-ens_start+1);
+     for idx = 1:enssembleNum
+         frmI_rmvDC(:,:,idx) = frmI(:,:,idx)-SumI;
+         frmQ_rmvDC(:,:,idx) = frmQ(:,:,idx)-SumQ;
+     end
+     %Calculate Modulus
+    PreEst_re = zeros(sampleCnt,lineCnt);
+    PreEst_im = zeros(sampleCnt,lineCnt);
+    for j=1:line_density:lineCnt
+        for idx=ens_start:enssembleNum-1
+            PreEst_re(:,j) =  PreEst_re(:,j) + frmI_rmvDC(:,j,idx) .* frmI_rmvDC(:,j,idx+1) + frmQ_rmvDC(:,j,idx) .* frmQ_rmvDC(:,j,idx+1);
+            PreEst_im(:,j) =  PreEst_im(:,j) + frmI_rmvDC(:,j,idx) .* frmQ_rmvDC(:,j,idx+1) - frmQ_rmvDC(:,j,idx) .* frmI_rmvDC(:,j,idx+1);
+        end
+    end
+    Modulus = ;
 %% Polynomial WallFilter 
-%     x=reshape(1:enssembleNum,1,1,enssembleNum);
-%     n = WF_Order;
-%     frmI_filtered = zeros(sampleCnt, lineCnt, enssembleNum);
-%     frmQ_filtered = zeros(sampleCnt, lineCnt, enssembleNum);
-%     for j=1:line_density:lineCnt
-%         for i=1:sampleCnt
-%             p = polyfit(x,frmI(i,j,:),n);
-%             fI = polyval(p,x,n);
-%             frmI_filtered(i,j,:) =  frmI(i,j,:) - fI;
-% 
-%             p = polyfit(x,frmQ(i,j,:),n);
-%             fQ = polyval(p,x,n);
-%             frmQ_filtered(i,j,:) =  frmQ(i,j,:) - fQ;
-%         end
-%     end
-%     save ([pathname 'tempdata',num2str(idx),'.mat'], 'frmI_filtered', 'frmQ_filtered')
+    x=reshape(1:enssembleNum,1,1,enssembleNum);
+    n = WF_Order;
+    frmI_filtered = zeros(sampleCnt, lineCnt, enssembleNum);
+    frmQ_filtered = zeros(sampleCnt, lineCnt, enssembleNum);
+    for j=1:line_density:lineCnt
+        for i=1:sampleCnt
+            if (Modulus(i,j)<Modulus_thre)
+                p = polyfit(x,frmI(i,j,:),n);
+                fI = polyval(p,x,n);
+                frmI_filtered(i,j,:) =  frmI(i,j,:) - fI;
     
-    load ([pathname 'tempdata',num2str(idx),'.mat'])
-%% Autocorrelation
+                p = polyfit(x,frmQ(i,j,:),n);
+                fQ = polyval(p,x,n);
+                frmQ_filtered(i,j,:) =  frmQ(i,j,:) - fQ;
+            end
+        end
+    end
+    save ([pathname 'tempdata',num2str(frm_idx),'.mat'], 'frmI_filtered', 'frmQ_filtered')
+    
+    load ([pathname 'tempdata',num2str(frm_idx),'.mat'])
+
+%% Autocorrelation and Normalized to 8bit(-128 ~ +127)
     re_auto = zeros(sampleCnt,lineCnt);
     im_auto = zeros(sampleCnt,lineCnt);
     for j=1:line_density:lineCnt
-         for idx=1:enssembleNum-1
+         for idx=ens_start:enssembleNum-1
              re_auto(:,j) = re_auto(:,j) + frmI_filtered(:,j,idx) .* frmI_filtered(:,j,idx+1) + frmQ_filtered(:,j,idx) .* frmQ_filtered(:,j,idx+1);
              im_auto(:,j) = im_auto(:,j) + frmI_filtered(:,j,idx) .* frmQ_filtered(:,j,idx+1) - frmQ_filtered(:,j,idx) .* frmI_filtered(:,j,idx+1);
          end
     end
-    v_est = c*fprf*cos(theta)/(4*pi*f0).* atan(im_auto./(re_auto+1e-6));
-
-    if (line_density~=1)
-        for j=2:line_density:lineCnt-1
-            v_est(:,j) = (v_est(:,j-1)+v_est(:,j+1))./2;
-        end
-    end
-
-%% Compress to 8bit(-128 ~ +127)
-    frmV = floor(32768.*100.*v_est./scale);
-%     frmV(abs(frmV)<ROthre) = 0;
-    velocity_frms(:,:,idx) = velocity_Compress(frmV, DB_range, 0.01);
+    v_est = atan2(im_auto(:,1:line_density:end),re_auto(:,1:line_density:end)).*127/pi;
+    velocity_frms(:,:,frm_idx) = int8(round(v_est));
 %% FlashReject
-    velocity_8bit = FlashReject_UIS( velocity_frms, idx, FRthre);
-%     a = 0.2;
-%     if idx>1
-%         R0_Prev = velocity_frms(:,:,idx-1);
-%         velocity_8bit = velocity_frms(:,:,idx) *a + R0_Prev*(1-a);
-%     else
-%         velocity_8bit = velocity_frms(:,:,idx);
-%     end
+    velocity_8bit = FlashReject_UIS( velocity_frms, frm_idx, FR_thre);
 
 %% smooth
-    velocity_8bit = medfilt2(velocity_8bit,[5,5]);
+%     velocity_8bit = medfilt2(velocity_8bit,[5,5]);
     
 %% scan conversion
     for i=1:sampleCnt
-        for j=1:lineCnt
+        for j=1:floor(lineCnt/line_density)
             CoordX = j+floor((sampleCnt-i)*sin(theta));
             velocity_8bitSC(i,CoordX) = velocity_8bit(i,j);
         end
     end
 %% display images
-    figure(2);
+    figure(1);
     image(double(velocity_8bitSC)+128);
     colormap(cfm_clrmap)
-    colorbar('Ticks',[1,256],...
+    colorbar('Ticks',[1,255],...
          'TickLabels',{['-',num2str(scale),' cm/s'],['+',num2str(scale),' cm/s']})
 end     
         
